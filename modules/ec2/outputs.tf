93158d6876ec45dd80f970dd466e730a07b7a621
Abdi-Shakur457 <abdishakur457@hotmail.com>
2026-03-25 21:10:51 +0000

    debugged cloud-init issues

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,11 +1,11 @@
 output "instance_ID" {
   description = "The ID of my wordpress ec2 instance"
-  value = aws_instance.wordpress_server.id 
+  value = aws_instance.Wordpress_server.id
   
 }
 
 output "EC2_Public_IP" {
   description = "The public IP of the wordpress instance"
-  value = aws_instance.wordpress_server.public_ip
+  value = aws_instance.Wordpress_server.public_ip
 }
   
